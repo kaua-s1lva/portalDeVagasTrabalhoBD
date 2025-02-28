97965cdfe67538d2f70f745114a784a5aaf80eaf
Tetzdesen <Tetzdesen@gmail.com>
2025-02-28 18:49:00 -0300

    Atualização script ddl

--- ddl.sql
+++ ddl.sql
@@ -1,9 +1,5 @@
-CREATE SCHEMA IF NOT EXISTS bdportalvagas;
-
-SET SCHEMA 'bdportalvagas';
-
 -- Tabela usuario
-CREATE TABLE IF NOT EXISTS bdportalvagas.usuario (
+CREATE TABLE IF NOT EXISTS usuario (
   idUsuario INT PRIMARY KEY,
   nome VARCHAR(1000) NOT NULL,
   email VARCHAR(1000) NOT NULL,
@@ -13,8 +9,36 @@ CREATE TABLE IF NOT EXISTS bdportalvagas.usuario (
   deleted_at TIMESTAMP
 );
 
+-- Tabela empresa
+CREATE TABLE IF NOT EXISTS empresa (
+  idEmpresa INT PRIMARY KEY,
+  cnpj CHAR(14) NOT NULL,
+  CONSTRAINT fk_empresa_usuario1 FOREIGN KEY (idEmpresa)
+    REFERENCES usuario (idUsuario) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Tabela aluno
+CREATE TABLE IF NOT EXISTS aluno (
+  idAluno INT PRIMARY KEY,
+  cpf CHAR(11) NOT NULL,
+  CONSTRAINT fk_aluno_usuario1 FOREIGN KEY (idAluno)
+    REFERENCES usuario (idUsuario) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Tabela egresso
+CREATE TABLE IF NOT EXISTS egresso (
+  idEgresso INT PRIMARY KEY,
+  idEmpresa INT NOT NULL,
+  cpf CHAR(11) NOT NULL,
+  CONSTRAINT fk_egresso_usuario1 FOREIGN KEY (idEgresso)
+    REFERENCES usuario (idUsuario) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT fk_egresso_empresa1 FOREIGN KEY (idEmpresa)
+    REFERENCES empresa (idEmpresa) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
 -- Tabela etapa
-CREATE TABLE IF NOT EXISTS bdportalvagas.etapa (
+CREATE TABLE IF NOT EXISTS etapa (
   idEtapa INT PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
   descricao TEXT NOT NULL,
@@ -22,16 +46,8 @@ CREATE TABLE IF NOT EXISTS bdportalvagas.etapa (
   updated_at TIMESTAMP
 );
 
--- Tabela empresa
-CREATE TABLE IF NOT EXISTS bdportalvagas.empresa (
-  idEmpresa INT PRIMARY KEY,
-  cnpj CHAR(14) NOT NULL,
-  CONSTRAINT fk_empresa_usuario1 FOREIGN KEY (idEmpresa)
-    REFERENCES bdportalvagas.usuario (idUsuario) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-
 -- Tabela vaga
-CREATE TABLE IF NOT EXISTS bdportalvagas.vaga (
+CREATE TABLE IF NOT EXISTS vaga (
   idVaga INT PRIMARY KEY,
   etapa_idEtapa INT NOT NULL,
   created_at TIMESTAMP NOT NULL,
@@ -40,28 +56,20 @@ CREATE TABLE IF NOT EXISTS bdportalvagas.vaga (
   cargo VARCHAR(1000) NOT NULL,
   empresa_idEmpresa INT NOT NULL,
   CONSTRAINT fk_vaga_status1 FOREIGN KEY (etapa_idEtapa)
-    REFERENCES bdportalvagas.etapa (idEtapa) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    REFERENCES etapa (idEtapa) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT fk_vaga_empresa1 FOREIGN KEY (empresa_idEmpresa)
-    REFERENCES bdportalvagas.empresa (idEmpresa) ON DELETE NO ACTION ON UPDATE NO ACTION
+    REFERENCES empresa (idEmpresa) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Tabela situacao
-CREATE TABLE IF NOT EXISTS bdportalvagas.situacao (
+CREATE TABLE IF NOT EXISTS situacao (
   idSituacao INT PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
   descricao TEXT NOT NULL
 );
 
--- Tabela aluno
-CREATE TABLE IF NOT EXISTS bdportalvagas.aluno (
-  idAluno INT PRIMARY KEY,
-  cpf CHAR(11) NOT NULL,
-  CONSTRAINT fk_aluno_usuario1 FOREIGN KEY (idAluno)
-    REFERENCES bdportalvagas.usuario (idUsuario) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-
 -- Tabela candidatura
-CREATE TABLE IF NOT EXISTS bdportalvagas.candidatura (
+CREATE TABLE IF NOT EXISTS candidatura (
   idVaga INT NOT NULL,
   aluno_idAluno INT NOT NULL,
   curriculo BYTEA NOT NULL,
@@ -71,33 +79,22 @@ CREATE TABLE IF NOT EXISTS bdportalvagas.candidatura (
   situacao_idSituacao INT NOT NULL,
   PRIMARY KEY (idVaga, aluno_idAluno),
   CONSTRAINT fk_usuario_has_vaga_vaga1 FOREIGN KEY (idVaga)
-    REFERENCES bdportalvagas.vaga (idVaga) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    REFERENCES vaga (idVaga) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT fk_candidatura_status1 FOREIGN KEY (situacao_idSituacao)
-    REFERENCES bdportalvagas.situacao (idSituacao) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    REFERENCES situacao (idSituacao) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT fk_candidatura_aluno1 FOREIGN KEY (aluno_idAluno)
-    REFERENCES bdportalvagas.aluno (idAluno) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-
--- Tabela egresso
-CREATE TABLE IF NOT EXISTS bdportalvagas.egresso (
-  idEgresso INT PRIMARY KEY,
-  idEmpresa INT NOT NULL,
-  cpf CHAR(11) NOT NULL,
-  CONSTRAINT fk_egresso_usuario1 FOREIGN KEY (idEgresso)
-    REFERENCES bdportalvagas.usuario (idUsuario) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT fk_egresso_empresa1 FOREIGN KEY (idEmpresa)
-    REFERENCES bdportalvagas.empresa (idEmpresa) ON DELETE NO ACTION ON UPDATE NO ACTION
+    REFERENCES aluno (idAluno) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Tabela status
-CREATE TABLE IF NOT EXISTS bdportalvagas.status (
+CREATE TABLE IF NOT EXISTS status (
   idstatus INT PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
   descricao TEXT NOT NULL
 );
 
 -- Tabela indicacao
-CREATE TABLE IF NOT EXISTS bdportalvagas.indicacao (
+CREATE TABLE IF NOT EXISTS indicacao (
   egresso_idEgresso INT NOT NULL,
   aluno_idAluno INT NOT NULL,
   vaga_idVaga INT NOT NULL,
@@ -107,29 +104,29 @@ CREATE TABLE IF NOT EXISTS bdportalvagas.indicacao (
   status_idStatus INT NOT NULL,
   PRIMARY KEY (egresso_idEgresso, aluno_idAluno, vaga_idVaga),
   CONSTRAINT fk_indicacao_egresso1 FOREIGN KEY (egresso_idEgresso)
-    REFERENCES bdportalvagas.egresso (idEgresso) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    REFERENCES egresso (idEgresso) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT fk_indicacao_aluno1 FOREIGN KEY (aluno_idAluno)
-    REFERENCES bdportalvagas.aluno (idAluno) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    REFERENCES aluno (idAluno) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT fk_indicacao_vaga1 FOREIGN KEY (vaga_idVaga)
-    REFERENCES bdportalvagas.vaga (idVaga) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    REFERENCES vaga (idVaga) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT fk_indicacao_status1 FOREIGN KEY (status_idStatus)
-    REFERENCES bdportalvagas.status (idstatus) ON DELETE NO ACTION ON UPDATE NO ACTION
+    REFERENCES status (idstatus) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Tabela requisito
-CREATE TABLE IF NOT EXISTS bdportalvagas.requisito (
+CREATE TABLE IF NOT EXISTS requisito (
   idRequisito INT PRIMARY KEY,
   nome VARCHAR(1000) NOT NULL,
   duracao VARCHAR(1000)
 );
 
 -- Tabela vaga_requisito
-CREATE TABLE IF NOT EXISTS bdportalvagas.vaga_requisito (
+CREATE TABLE IF NOT EXISTS vaga_requisito (
   vaga_idVaga INT NOT NULL,
   requisito_idRequisito INT NOT NULL,
   PRIMARY KEY (vaga_idVaga, requisito_idRequisito),
   CONSTRAINT fk_vaga_has_requisito_vaga1 FOREIGN KEY (vaga_idVaga)
-    REFERENCES bdportalvagas.vaga (idVaga) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    REFERENCES vaga (idVaga) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT fk_vaga_has_requisito_requisito1 FOREIGN KEY (requisito_idRequisito)
-    REFERENCES bdportalvagas.requisito (idRequisito) ON DELETE NO ACTION ON UPDATE NO ACTION
+    REFERENCES requisito (idRequisito) ON DELETE CASCADE ON UPDATE CASCADE
 );
