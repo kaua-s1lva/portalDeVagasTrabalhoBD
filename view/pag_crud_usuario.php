2063d5501ad7b2c98f43233b3b09ab6eed93d338
Flávio Monteiro <fla.mont2019@gmail.com>
2025-03-01 16:34:48 -0300

    criação html/css das páginas de CRUD de usuário e empresa

---
+++ view/pag_crud_usuario.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../styles/pag_crud_usuario.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+      rel="stylesheet"
+    />
+    <title>Manter Usuário</title>
+  </head>
+  <body>
+    <aside>
+      <div class="vagas-usu-img">
+        <img src="../assets/ufes-logo.png" alt="ufes-logo" />
+      </div>
+      <div class="links">
+        <a href="">Perfil</a>
+        <a href="">Visualizar Vagas</a>
+        <a href="">Log Off</a>
+      </div>
+    </aside>
+    <main>
+      <section class="header">
+        <h1>Atualizar Dados Pessoais</h1>
+      </section>
+      <section class="container">
+        <form id="loginForm">
+          <div class="crud-form-input">
+            <div class="inplbl">
+              <label for="username">Nome:</label>
+              <input type="text" id="username" name="username" required />
+            </div>
+            <div class="inplbl">
+              <label for="password">E-mail:</label>
+              <input type="password" id="password" name="password" required />
+            </div>
+            <div class="inplbl">
+              <label for="username">Senha:</label>
+              <input type="text" id="username" name="username" required />
+            </div>
+            <div class="inplbl">
+              <label for="username">CPF:</label>
+              <input type="text" id="username" name="username" required />
+            </div>
+          </div>
+          <div class="crud-form-buttons">
+            <button type="submit">Salvar</button>
+            <button type="submit">Excluir</button>
+          </div>
+        </form>
+      </section>
+    </main>
+  </body>
+</html>
