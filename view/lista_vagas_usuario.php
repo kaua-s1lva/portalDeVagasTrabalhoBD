94a0564f00621ea9d3d3000990a7dd3b4018a1be
Flávio Monteiro <fla.mont2019@gmail.com>
2025-03-01 14:19:57 -0300

    update de css lista-vagas-usuario

--- view/lista_vagas_usuario.php
+++ view/lista_vagas_usuario.php
@@ -4,19 +4,28 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="../styles/lista_vagas_usuario.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+      rel="stylesheet"
+    />
     <title>Vagas disponíveis</title>
   </head>
   <body>
     <aside>
-      <img src="../assets/ufes-logo.png" alt="" />
-      <a href="">Perfil</a>
-      <span></span>
-      <a href="">Gerência de Vagas</a>
+      <div class="vagas-usu-img">
+        <img src="../assets/ufes-logo.png" alt="" />
+      </div>
+      <div class="links">
+        <a href="">Perfil</a>
+        <a href="">Visualizar Vagas</a>
+      </div>
     </aside>
     <main>
-      <section>
+      <section class="header">
         <h1>Vagas disponíveis</h1>
-        <button>Refresh</button>
+        <button>Update</button>
       </section>
       <section class="container">
         <table>
@@ -33,7 +42,6 @@
               <td>Stacks LTDA</td>
               <td class="buttons">
                 <button>Candidatar-se</button>
-                <button>Salvar Vaga</button>
               </td>
             </tr>
             <tr>
@@ -41,7 +49,6 @@
               <td>Unreal Data</td>
               <td class="buttons">
                 <button>Candidatar-se</button>
-                <button>Salvar Vaga</button>
               </td>
             </tr>
           </tbody>
